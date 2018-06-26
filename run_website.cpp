6b17de221ad2b1d0a4ea23ea28ec1518253d3d7b
TwITe <dm.blink19@gmail.com>
2018-06-26 23:46:50 +0300

    get goods handlers are done

--- run_website.cpp
+++ run_website.cpp
@@ -4,52 +4,26 @@
 
 const unsigned short int PORT = 8080;
 
-map<string, string> generate_default_goods() {
-    map<string, string> goods;
-
-    for (int i = 0; i < 5; i++) {
-        goods["default" + to_string(i)] = to_string(i * 30);
-    }
-
-    return goods;
-}
-
-map<string, string> generate_user_goods(const string& name) {
-    map<string, string> goods;
-
-    int a = 0;
-
-    for (unsigned int i = 0; i < name.length(); i++) {
-        a += name[i];
-    }
-
-    for (unsigned int i = 0; i < name.length(); i++) {
-        goods["name" + to_string(i)] = to_string((i + 1) * a);
-    }
-
-    return goods;
-}
-
 int main() {
     vector<webserver::web_handler> handlers;
 
-    _Pragma("GCC diagnostic push")
-    _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
     function<webserver::http_response(webserver::http_request)> put_goods = [&](webserver::http_request request) {
         webserver::http_response response;
         string response_body;
 
+        const unsigned int default_goods_number = 5;
         string user_name;
 
-        const vector<webserver::request_param> request_parameters = request.get_request_params();
-        for (auto current_request_parameter : request_parameters) {
-            if (current_request_parameter.name == "name") {
-                user_name = current_request_parameter.value;
-                break;
+        if (request.get_request_method() == "GET") {
+            const vector<webserver::request_param> request_parameters = request.get_request_params();
+            for (auto current_request_parameter : request_parameters) {
+                if (current_request_parameter.name == "name") {
+                    user_name = current_request_parameter.value;
+                    break;
+                }
             }
         }
-
-        if (user_name.empty()) {
+        else {
             const map<string, string>& request_body = request.get_request_body();
             for (auto current_param : request_body) {
                 if (current_param.first == "name") {
@@ -59,24 +33,36 @@ int main() {
             }
         }
 
-        map<string, string> goods;
-
         nlohmann::json j;
 
         if (!user_name.empty()) {
-            goods = generate_user_goods(user_name);
+            //fill with particular goods for current user
             j["nickname"] = user_name;
+
+            int a = 0;
+            for (unsigned int i = 0; i < user_name.length(); i++) {
+                a += user_name[i];
+            }
+
+            for (unsigned int i = 0; i < user_name.length(); i++) {
+                j["items"][i]["name"] = user_name + to_string(i);
+                j["items"][i]["price"] = to_string((i + 1) * a);
+            }
         }
         else {
-            goods = generate_default_goods();
+            //fill with default goods
+            for (int i = 0; i < 5; i++) {
+                for (unsigned int i = 0; i < default_goods_number; i++) {
+                    j["items"][i]["name"] = "default" + to_string(i);
+                    j["items"][i]["price"] = to_string(i * 30);
+                }
+            }
         }
 
-        nlohmann::json j_goods(goods);
+        response_body = j.dump();
 
-        j["items"] = j_goods;
-
-        response.set_response_body(response_body);
         response.set_response_http_code(200);
+        response.set_response_body(response_body);
         response.set_response_length(response_body.size());
 
         vector<webserver::http_header> headers;
@@ -85,9 +71,12 @@ int main() {
 
         return response;
     };
-    _Pragma("GCC diagnostic pop")
 
-    webserver::web_handler get_goods("/", "GET", put_goods);
+    webserver::web_handler get_goods_get_request("/", "GET", put_goods);
+    webserver::web_handler get_goods_post_request("/", "POST", put_goods);
+
+    handlers.emplace_back(get_goods_get_request);
+    handlers.emplace_back(get_goods_post_request);
 
     webserver::web_server server(PORT, handlers);
 
