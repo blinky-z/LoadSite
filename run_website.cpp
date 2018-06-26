1374ea8c5dc16b1ab3c3acf9b69eea30069e7f28
TwITe <dm.blink19@gmail.com>
2018-06-27 01:32:59 +0300

    add goods buy handler

--- run_website.cpp
+++ run_website.cpp
@@ -23,7 +23,7 @@ int main() {
                 }
             }
         }
-        else {
+        if (request.get_request_method() == "POST") {
             const map<string, string>& request_body = request.get_request_body();
             for (auto current_param : request_body) {
                 if (current_param.first == "name") {
@@ -33,33 +33,34 @@ int main() {
             }
         }
 
-        nlohmann::json j;
+        nlohmann::json json_body;
 
         if (!user_name.empty()) {
             //fill with particular goods for current user
-            j["nickname"] = user_name;
+            json_body["nickname"] = user_name;
 
-            int a = 0;
-            for (unsigned int i = 0; i < user_name.length(); i++) {
-                a += user_name[i];
+            unsigned int multiplier = 0;
+            for (unsigned int current_char_number = 0; current_char_number < user_name.length(); current_char_number++) {
+                multiplier += user_name[current_char_number];
             }
 
-            for (unsigned int i = 0; i < user_name.length(); i++) {
-                j["items"][i]["name"] = user_name + to_string(i);
-                j["items"][i]["price"] = to_string((i + 1) * a);
+            for (unsigned int current_good_number = 0; current_good_number < user_name.length(); current_good_number++) {
+                json_body["items"][current_good_number]["name"] = user_name + to_string(current_good_number);
+                json_body["items"][current_good_number]["price"] = to_string((current_good_number + 1) * multiplier);
             }
         }
         else {
             //fill with default goods
-            for (int i = 0; i < 5; i++) {
-                for (unsigned int i = 0; i < default_goods_number; i++) {
-                    j["items"][i]["name"] = "default" + to_string(i);
-                    j["items"][i]["price"] = to_string(i * 30);
-                }
+            unsigned int multiplier = 30;
+
+            for (unsigned int current_good_number = 0; current_good_number < default_goods_number; current_good_number++) {
+                json_body["items"][current_good_number]["name"] = "default" + to_string(current_good_number);
+                json_body["items"][current_good_number]["price"] = to_string(current_good_number * multiplier);
             }
         }
 
-        response_body = j.dump();
+        //get string representation of the json body
+        response_body = json_body.dump();
 
         response.set_response_http_code(200);
         response.set_response_body(response_body);
@@ -75,8 +76,43 @@ int main() {
     webserver::web_handler get_goods_get_request("/", "GET", put_goods);
     webserver::web_handler get_goods_post_request("/", "POST", put_goods);
 
+    function<webserver::http_response(webserver::http_request)> buy_goods_check = [&](webserver::http_request request) {
+        webserver::http_response response;
+
+        nlohmann::json json_response_body;
+        string success_purchase_message = "success";
+        string failure_purchase_message = "failure";
+
+        const map<string, string>& request_body = request.get_request_body();
+
+        string raw_json_body = request_body.begin()->second;
+
+        nlohmann::json json_request_body = nlohmann::json::parse(raw_json_body);
+
+        string good_name = json_request_body["name"];
+
+        if (good_name.length() % 2 == 0) {
+            json_response_body["result"] = success_purchase_message;
+        }
+        else {
+            json_response_body["result"] = failure_purchase_message;
+        }
+
+        //get string representation of the json body
+        string response_body = json_response_body.dump();
+
+        response.set_response_http_code(200);
+        response.set_response_body(response_body);
+        response.set_response_length(response_body.size());
+
+        return response;
+    };
+
+    webserver::web_handler buy_goods("/buy", "POST", buy_goods_check);
+
     handlers.emplace_back(get_goods_get_request);
     handlers.emplace_back(get_goods_post_request);
+    handlers.emplace_back(buy_goods);
 
     webserver::web_server server(PORT, handlers);
 
